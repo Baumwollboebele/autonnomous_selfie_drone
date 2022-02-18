65334efb880a69082104227100b36435d0b25fd3
Baumwollboebele <33659763+Baumwollboebele@users.noreply.github.com>
2022-02-18 12:44:44 +0100

    Update test_PoseDetection.py

--- test/test_PoseDetection.py
+++ test/test_PoseDetection.py
@@ -13,9 +13,9 @@ class TestPoseDetection(unittest.TestCase):
     self.assertEqual(self.pd.left_arm_up(40,50,60,50),False)
     
   def test_right_arm_up(self):
-    self.assertEqual(self.pd.left_arm_up(0,0,0,0),False)
-    self.assertEqual(self.pd.left_arm_up(60,50,40,50),False)
-    self.assertEqual(self.pd.left_arm_up(40,50,60,50),True)
+    self.assertEqual(self.pd.right_arm_up(0,0,0,0),False)
+    self.assertEqual(self.pd.right_arm_up(60,50,40,50),False)
+    self.assertEqual(self.pd.right_arm_up(40,50,60,50),True)
     
     
 
