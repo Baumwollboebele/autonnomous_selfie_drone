10455448c617a309d9c095917389a448f2aed964
Baumwollboebele <33659763+Baumwollboebele@users.noreply.github.com>
2022-02-18 11:19:02 +0100

    Create test_PoseDetection.py
    added unittest for pose detecion

---
+++ test/test_PoseDetection.py
@@ -0,0 +1,16 @@
+import unittest
+
+class TestPoseDetection(unittest.TestCase):
+  
+  def test_left_arm_up(self):
+    self.assertEqual(True,True)
+    
+  def test_right_arm_up(self):
+    self.assertEqual(True,True)
+    
+    
+    
+    
+if __name__ == '__main__':
+    unittest.main()
+    
