47701464ad36c491de07f319ffde529895ac9fb8
Baumwollboebele <33659763+Baumwollboebele@users.noreply.github.com>
2022-02-18 21:14:22 +0100

    update

--- test/test_PoseDetection.py
+++ test/test_PoseDetection.py
@@ -1,23 +1,31 @@
-import unittest
+import pytest
 from source.PoseDetection import PoseDetection
 
-class TestPoseDetection(unittest.TestCase):
+class TestPoseDetection():
+  """
+  Testing class for the PoseDetection Module
+  """
+  @pytest.fixture
+  def pose_detection(self):
+    return PoseDetection()
   
-  def setUp(self):
-    self.pd = PoseDetection()
-  
-  def test_left_arm_up(self):
-    
-    self.assertEqual(self.pd.left_arm_up(0,0,0,0),False)
-    self.assertEqual(self.pd.left_arm_up(60,50,40,50),True)
-    self.assertEqual(self.pd.left_arm_up(40,50,60,50),False)
-    
-  def test_right_arm_up(self):
-    self.assertEqual(self.pd.right_arm_up(0,0,0,0),False)
-    self.assertEqual(self.pd.right_arm_up(60,50,40,50),False)
-    self.assertEqual(self.pd.right_arm_up(40,50,60,50),True)
-    
-    
+  def test_left_arm_up(self,pose_detection):
+    """Testmethod for the function left_arm_up in the PoseDetection Module.
 
-if __name__ == '__main__':
-    unittest.main()
+    Args:
+        pose_detection (PoseDetection): PoseDetection Object
+    """
+    assert pose_detection.left_arm_up(0, 0, 0, 0) == False
+    assert pose_detection.left_arm_up(60, 50, 40, 50) == True
+    assert pose_detection.left_arm_up(40, 50, 60, 50) == False
+  
+
+  def test_right_arm_up(self, pose_detection):
+    """Testmethod for the function right_arm_up in the PoseDetection Module.
+
+    Args:
+        pose_detection (PoseDetection): PoseDetection Object
+    """
+    assert pose_detection.right_arm_up(0, 0, 0, 0) == False
+    assert pose_detection.right_arm_up(60, 50, 40, 50) == False
+    assert pose_detection.right_arm_up(40, 50, 60, 50) == True
