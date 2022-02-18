ccdf93d66a314eabebb93bd43a5c37639505e69a
Baumwollboebele <33659763+Baumwollboebele@users.noreply.github.com>
2022-02-18 12:22:31 +0100

    Update test_PoseDetection.py

--- test/test_PoseDetection.py
+++ test/test_PoseDetection.py
@@ -3,8 +3,11 @@ from source.PoseDetection import PoseDetection
 
 class TestPoseDetection(unittest.TestCase):
   
+  def __init__(self):
+    self.pose_detection = PoseDetection()
+  
   def test_left_arm_up(self):
-    self.assertEqual(PoseDetection.left_arm_up(0,0,0,0),False)
+    self.assertEqual(pose_detection.left_arm_up(0,0,0,0),False)
     
   def test_right_arm_up(self):
     self.assertEqual(True,True)
